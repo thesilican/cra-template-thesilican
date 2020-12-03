e37a65bbbd0817f8418f92120cf4c1a48b037412
Bryan Chen <bryanchen74@gmail.com>
2020-12-03 03:48:38 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# cra-template-template
+
+My own personal create react app template. Forked from [cra-template-typescript](https://github.com/facebook/create-react-app/tree/master/packages/cra-template-typescript).
---
+++ template/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./styles/App.module.scss";
+
+export default function App() {
+  return (
+    <div className={styles.App}>
+      <h1>Hello, World!</h1>
+    </div>
+  );
+}
